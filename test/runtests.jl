299944add172d675e7c9e9e13764b873c2832297
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-03-20 15:38:53 +0100

    Test everythings ;-)

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,15 @@
+module TestingAsType
+
 using AsType
 using Test
 
 @testset "AsType.jl" begin
-    # Write your tests here.
+    @test as(Int, 3) === 3
+    @test as(Int, 1.0) === 1
+    @test as(Int16, 1.0) === Int16(1)
+    @test_throws InexactError as(Int, sqrt(2))
+    @test_throws Exception as(Int, π)
+    @test map(as(Int), (Int8(1), Int16(2), Int32(3), Int64(4))) === (1,2,3,4)
 end
+
+end # module
