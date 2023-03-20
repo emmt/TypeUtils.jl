770d6c88c4c0bf1a908895929f881f728dab5a34
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-03-20 15:35:52 +0100

    Implementation

--- src/AsType.jl
+++ src/AsType.jl
@@ -1,5 +1,24 @@
 module AsType
 
-# Write your package code here.
+export as
+
+"""
+    as(T, x)
+
+yields `x` converted to type `T`.
+
+"""
+as(::Type{T}, x::T) where {T} = x
+as(::Type{T}, x) where {T} = convert(T, x)::T
+
+"""
+    as(T)
+
+yields a callable object which converts its argument to type `T`.
+
+"""
+as(::Type{T}) where {T} = As{T}()
+struct As{T} <: Function; end
+(::As{T})(x) where {T} = as(T, x)
 
 end
