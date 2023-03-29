ea2a701d23440ac3aa1ac6b5318b825c47a4195f
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-03-29 08:44:35 +0200

    Extend compatibility to Julia ≥ 0.7

--- src/AsType.jl
+++ src/AsType.jl
@@ -38,14 +38,14 @@ struct As{T} <: Function; end
 
 function __init__()
     @require TwoDimensional="1907e7ba-7586-4310-a2ba-dd01462aeb50" begin
-        for (type,N,T) in ((:(TwoDimensional.Point), 2, Real),
-                           (:(TwoDimensional.WeightedPoint), 3, AbstractFloat),
-                           (:(TwoDimensional.BoundingBox), 4, Real),)
+        for (X,N,T) in ((:(TwoDimensional.Point), 2, Real),
+                        (:(TwoDimensional.WeightedPoint), 3, AbstractFloat),
+                        (:(TwoDimensional.BoundingBox), 4, Real),)
             @eval begin
-                as(::Type{$type}, x::NTuple{$N,$T}) = $type(x...)
-                as(::Type{$type{T}}, x::NTuple{$N,$T}) where {T} = $type{T}(x...)
-                as(::Type{Tuple}, x::$type) = Tuple(x)
-                as(::Type{NTuple{$N,T}}, x::$type) where {T<:$T} = map(as(T), Tuple(x))
+                as(::Type{$X}, x::NTuple{$N,$T}) = $X(x...)
+                as(::Type{$X{T}}, x::NTuple{$N,$T}) where {T} = $X{T}(x...)
+                as(::Type{Tuple}, x::$X) = Tuple(x)
+                as(::Type{NTuple{$N,T}}, x::$X) where {T<:$T} = map(as(T), Tuple(x))
             end
         end
     end
