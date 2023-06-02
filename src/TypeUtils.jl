e3a95aafa8cc6595d605d81fcb735d1095e212fc
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2023-06-02 11:52:33 +0200

    Update doc., inline function

--- src/TypeUtils.jl
+++ src/TypeUtils.jl
@@ -19,7 +19,7 @@ as(::Type{Tuple}, x::CartesianIndices) = x.indices
 for X in (:CartesianIndex, :CartesianIndices)
     @eval begin
         # For more specific tuple types, first extract tuple contents, then
-        # convert to the specific tuple type.
+        # convert to the requested tuple type.
         as(::Type{T}, x::$X) where {T<:Tuple} = as(T, as(Tuple, x))
 
         # Use the constructors to convert tuples to Cartesian index/indices.
@@ -33,14 +33,15 @@ as(::Type{String}, x::Symbol) = String(x)
 as(::Type{Symbol}, x::String) = Symbol(x)
 
 """
-    as(T)
+    f = as(T)
 
-yields a callable object which converts its argument to type `T`.
+yields a callable object which converts its argument to type `T`. More
+specifically, a call like `f(x)` yields `as(T, x)`.
 
 """
 as(::Type{T}) where {T} = As{T}()
 struct As{T} <: Function; end
-(::As{T})(x) where {T} = as(T, x)
+@inline (::As{T})(x) where {T} = as(T, x)
 
 function __init__()
     @require TwoDimensional="1907e7ba-7586-4310-a2ba-dd01462aeb50" begin
