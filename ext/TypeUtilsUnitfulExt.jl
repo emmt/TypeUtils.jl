38f016e380f2c88bc02788a0242a6d1f8a7302e2
Éric Thiébaut <eric.thiebaut@univ-lyon1.fr>
2025-06-19 12:02:47 +0200

    Make code more readable

--- ext/TypeUtilsUnitfulExt.jl
+++ ext/TypeUtilsUnitfulExt.jl
@@ -1,25 +1,24 @@
 module TypeUtilsUnitfulExt
 if isdefined(Base, :get_extension)
     using TypeUtils, Unitful
+    using TypeUtils: BareNumber
     using Unitful: AbstractQuantity, Quantity, ustrip, unit
 else
     using ..TypeUtils, ..Unitful
+    using ..TypeUtils: BareNumber
     using ..Unitful: AbstractQuantity, Quantity, ustrip, unit
 end
 
 
 # Extend bare_type, real_type, convert_bare_type, and convert_real_type.
-for (f, g, S) in ((:(TypeUtils.bare_type), :(TypeUtils.convert_bare_type), :(TypeUtils.BareNumber)),
+for (f, g, S) in ((:(TypeUtils.bare_type), :(TypeUtils.convert_bare_type), :BareNumber),
                   (:(TypeUtils.real_type), :(TypeUtils.convert_real_type), :Real))
     @eval begin
         $f(::Type{<:AbstractQuantity{T}}) where {T} = $f(T)
         $g(::Type{T}, x::AbstractQuantity{T}) where {T<:$S} = x
-        @inline $g(::Type{T}, x::AbstractQuantity) where {T<:$S} =
-            $g(T, ustrip(x))*unit(x)
-        $g(::Type{T}, ::Type{Quantity{T,D,U}}) where {T<:$S,D,U} =
-            Quantity{T,D,U}
-        @inline $g(::Type{T}, ::Type{Quantity{R,D,U}}) where {T<:$S,R,D,U} =
-            Quantity{$g(T,R),D,U}
+        $g(::Type{T}, x::AbstractQuantity   ) where {T<:$S} = $g(T, ustrip(x))*unit(x)
+        $g(::Type{T}, ::Type{Quantity{T,D,U}}) where {T<:$S,D,U} = Quantity{T,D,U}
+        $g(::Type{T}, ::Type{Quantity{R,D,U}}) where {T<:$S,D,U,R} = Quantity{$g(T,R),D,U}
     end
 end
 
